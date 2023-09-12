bd261dea6474cce716c484c8918e5f053739db43
Ouroboros-19 <nazargutsu@gmail.com>
2023-09-12 19:59:50 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Random r = new Random();
+        int a = 20, k;
+        int[] array = new int[a];
+        array[0] = r.Next(0, 20);
+        Console.Write("{0,3}", array[0]);
+        for (int i = 1; i < a; i++)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                array[i] = r.Next(0, 20);
+                k = 0;
+                for (int j = 0; j < i; j++)
+                    if (array[j] == array[i])
+                        k++;
+                if (k == 0)
+                    flag = false;
+            }
+            Console.Write("{0,3}", array[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine("");
+        Console.WriteLine("Повтор?( enter - да; люб. др. клавиша - нет )");
+        string povtor = Convert.ToString(Console.ReadLine());
+        if (povtor == "")
+            Main();
+    }
+}
